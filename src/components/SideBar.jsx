06856ff54f99e2fd25a1ebefc35919c2f7986151
Jacob Espinosa <jaespinosa03@outlook.com>
2026-06-18 12:04:39 -0700

    Further work on sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,23 @@
 function SideBar() {
   return (
-    <div className="border-2 border-black w-3xs h-screen ">
-       <p>SideBar</p>
-    </div>
+    <aside className="surface border-r-2 border-theme w-3xs h-screen">
+      <div className="h-16 p-4 text-center text-2xl">
+        <a href="#">App Name</a>
+      </div>
+
+      <div className="flex flex-col mt-2">
+        <p className="p-4 text-secondary">Menu</p>
+        <a href="#" className="border-1 p-4 text-primary text-lg">Dashboard</a>
+        <a href="#">Calendar</a>
+        <a href="#">Projects</a>
+        <a href="#">Analytics</a>
+      </div>
+
+      <div className="flex flex-col">
+        <p className="p-4 text-secondary">General</p>
+        <a href="#">Settings</a>
+      </div>
+    </aside>
   );
 }
 
